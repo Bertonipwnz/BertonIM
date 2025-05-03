3666e0e1424b2180f4f6ffbf0de4536f50f11933
Michail <70506606+Bertonipwnz@users.noreply.github.com>
2025-05-03 20:56:01 +0700

    Добавление моделей представления для страниц

---
+++ BertoniIM.UWP/ViewModels/Pages/ChatsPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BertoniIM.UWP.ViewModels.Pages
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	internal class ChatsPageViewModel
+	{
+	}
+}
---
+++ BertoniIM.UWP/ViewModels/Pages/ShellPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BertoniIM.UWP.ViewModels.Pages
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	internal class ShellPageViewModel
+	{
+	}
+}
